d444d8ab19bcf4ab173e0179d08ec673cc3fe5c8
adampeer <90769663+adampeer@users.noreply.github.com>
2021-09-29 13:57:13 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+let viz = 'scatter.vl.json';
+
+vegaEmbed('#scatter-area', viz, { "actions": false }).then(function (result) {
+
+}).catch(console.error);
+
+let viz1 = 'line.vl.json';
+
+vegaEmbed('#line-area', viz1, { "actions": false }).then(function (result) {
+
+}).catch(console.error);
